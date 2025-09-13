00d6b962df136bd9d3e6e6b199ba75eab35575f8
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-09-14 00:00:57 +0700

    Refactor layout and metadata configuration
    - Updated layout.tsx to import global styles from the new path and export metadata from a dedicated config file.
    - Created metadata.ts to define application metadata with a new title and description.
    - Added globals.css to import Tailwind CSS for styling.

---
+++ src/configs/metadata.ts
@@ -0,0 +1,7 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "FLOWN",
+  description:
+    "FLOWN is designed to streamline and simplify the approval workflow processes within organizations, making it easier for teams to manage, execute, and track workflows efficiently. The system focuses on user-centric management, customizable workflows, and flexible approval mechanisms to adapt to various business needs.",
+};
