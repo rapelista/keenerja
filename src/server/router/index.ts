f6d180e09e334b1e0b58b39693f6cac95d1cf39c
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-09-14 03:07:12 +0700

    feat: create tRPC configuration and update router import path

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,4 +1,4 @@
-import { publicProcedure, router } from "~/server/trpc";
+import { publicProcedure, router } from "~/configs/trpc";
 
 export const appRouter = router({
   greeting: publicProcedure.query(() => "hello tRPC v10!"),
