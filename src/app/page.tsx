e2d9b51919cc6088b575f53b8e06b41417a291bf
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-09-14 00:05:31 +0700

    feat: add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "~/components/ui/button";
+
 export default function Home() {
   return (
     <main>
       <div>Hello world!</div>
+
+      <Button>Click Me!</Button>
     </main>
   );
 }
