e5bcd988fff73b915eb0d8cc3f66b58ac2c1cf40
Farih Akmal Haqiqi <akmal@team.gamatecha.com>
2025-09-17 13:53:49 +0700

    feat: implement sign-up form with validation and submission logic

--- src/lib/mutations/auth.ts
+++ src/lib/mutations/auth.ts
@@ -1,8 +1,9 @@
 'use client';
 
 import { mutationOptions } from '@tanstack/react-query';
-import { SignInEmailPassword } from '~/schema/sign-in';
-import { signIn, signOut } from '../auth/client';
+import { SignInEmailPassword } from '~/schema/auth/sign-in';
+import { SignUpEmailPassword } from '~/schema/auth/sign-up';
+import { signIn, signOut, signUp } from '../auth/client';
 
 export const authMutations = {
   signIn: {
@@ -10,6 +11,16 @@ export const authMutations = {
       mutationOptions({
         mutationFn: async (payload: SignInEmailPassword) => {
           const { error } = await signIn.email(payload);
+          if (error) throw error;
+        },
+      }),
+  },
+
+  signUp: {
+    email: () =>
+      mutationOptions({
+        mutationFn: async (payload: SignUpEmailPassword) => {
+          const { error } = await signUp.email(payload);
           if (error) throw error;
         },
       }),
